c1d5ccd27abb0112a45a7b52336e9dbbdc4a10cc
xconnect <xconnect@ubuntu>
2012-06-21 02:30:51 +0200

    Angels

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,40 +17,40 @@ class BootStrap {
             //new Angle(name:"Bhf Berlin-Spandau", address:"Segefelder Straße 1",  
             //    post_code:"13597", city:"Berlin").save()
             
-            new Angle(name:"Carmeq (Berlin)", address:"Helmholtzstr., Berlin",  
+            new Angle(name:"CQ Büro (B)", address:"Helmholtzstr., Berlin",  
                 post_code:"10587", city:"Berlin",  vicinity:"Berlin").save()
 
             // Wolfsburg
-            new Angle(name:"Carmeq - Autovision (Wolfsburg)", address:"Forum AutoVision, Wolfsburg 11",  
+            new Angle(name:"CQ Autovision", address:"Forum AutoVision, Wolfsburg 11",  
                 post_code:"38442", city:"Wolfsburg",  vicinity:"Wolfsburg").save()
              
-            new Angle(name:"VW TE (Hopfengarten)", address:"Mozartstraße, Wolfsburg-Fallersleben",  
+            new Angle(name:"VW-TE Hopfengarten", address:"Mozartstraße, Wolfsburg-Fallersleben",  
                 post_code:"38442", city:"Wolfsburg",  vicinity:"Wolfsburg").save()
             
-            new Angle(name:"VW TE (Rübenkamp)", address:"Mozartstraße, Wolfsburg-Fallersleben",  
+            new Angle(name:"VW-TE Rübenkamp", address:"Mozartstraße, Wolfsburg-Fallersleben",  
                 post_code:"38442", city:"Wolfsburg",  vicinity:"Wolfsburg").save()
             
-            new Angle(name:"VW AutoUni/Mobile/LifeCampus", address:"Hageberg Brücke Sandkamp, Wolfsburg",  
+            new Angle(name:"VW AutoUni", address:"Hageberg Brücke Sandkamp, Wolfsburg",  
                 post_code:"38440", city:"Wolfsburg",  vicinity:"Wolfsburg").save()
             
-            new Angle(name:"VW FE (LKW Wache)", address:"VW LKW Waage, Wolfsburg",  
+            new Angle(name:"VW-FE LKW Wache", address:"VW LKW Waage, Wolfsburg",  
                 post_code:"38440", city:"Wolfsburg",  vicinity:"Wolfsburg").save()
             
-            new Angle(name:"VW FE (Fussgänger Wache)", address:"Saarstraße, Wolfsburg ",  
+            new Angle(name:"VW-FE FG Wache", address:"Saarstraße, Wolfsburg ",  
                 post_code:"38440", city:"Wolfsburg",  vicinity:"Wolfsburg").save()
 
-            new Angle(name:"VW HMI (Fa. Volke)", address:"AutoMuseum, Wolfsburg ",  
+            new Angle(name:"VW-HMI Volke", address:"AutoMuseum, Wolfsburg ",  
                 post_code:"38446", city:"Wolfsburg",  vicinity:"Wolfsburg").save()
             
             new Angle(name:"VW Isenbüttel", address:"Kl. Moorweg, Isenbüttel ",  
-                post_code:"38550", city:"Isenbüttel",  vicinity:"Isenbüttel").save()
+                post_code:"38550", city:"Isenbüttel",  vicinity:"Wolfsburg").save()
             
-            new Angle(name:"VW Prüfgelände Ehra(GEHT NICHT)", address:"",  
-                post_code:"38468", city:"Ehra-Lessien",  vicinity:"Ehra-Lessien").save()
+            new Angle(name:"VW Ehra (GEHT NICHT)", address:"Am Platz 1945",  
+                post_code:"38468", city:"Ehra-Lessien",  vicinity:"Wolfsburg").save()
             
             // Ingolstadt
-            new Angle(name:"Carmeq (Ingolstadt)", address:"Gewerbegebiet, Gaimersheim ",  
-                post_code:"85080", city:"Gaimersheim",  vicinity:"Gaimersheim").save()
+            new Angle(name:"CQ Büro (IN)", address:"Gewerbegebiet, Gaimersheim ",  
+                post_code:"85080", city:"Gaimersheim",  vicinity:"Ingolstadt").save()
             
             new Angle(name:"Audi TE", address:"Audi - Tor 10, Ingolstadt",  
                 post_code:"85055", city:"Ingolstadt",  vicinity:"Ingolstadt").save()
@@ -59,17 +59,17 @@ class BootStrap {
                 post_code:"85057", city:"Ingolstadt",  vicinity:"Ingoltstadt").save()
             
             // Stuttgart
-            new Angle(name:"Carmeq (Stuttgart)", address:"Schlossplatz, Stuttgart ",  
+            new Angle(name:"CQ Büro (S)", address:"Schlossplatz, Stuttgart ",  
                 post_code:"70173", city:"Stuttgart",  vicinity:"Stuttgart").save()
             
-            new Angle(name:"Porsche Weissach", address:"Weissach (Württ) Marktplatz ",  
-                post_code:"71287", city:"Weissach",  vicinity:"Weissbach").save()
+            new Angle(name:"Porsche", address:"Weissach (Württ) Marktplatz ",  
+                post_code:"71287", city:"Weissach",  vicinity:"Stuttgart").save()
             
             // Prag
-            new Angle(name:"e4t Prag(GEHT NICHT)", address:"Novodvorská 994/138",  
+            new Angle(name:"e4t Prag (GEHT NICHT)", address:"Novodvorská 994/138",  
                 post_code:"147 00", city:"Praha 4-Branîk",  vicinity:"Prag").save()
             
-            new Angle(name:"Skoda Werk(GEHT NICHT)", address:"Mladá Boleslav",  
+            new Angle(name:"Skoda Werk (GEHT NICHT)", address:"Mladá Boleslav",  
                 post_code:"", city:"CZ",  vicinity:"Prag").save()
         }
         
